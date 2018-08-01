ef3574b5b8ca6489f0281d427b21656d6ceb8ec5
Jesus Fuentes <jesusfuentes2808@gmail.com>
2018-08-01 10:16:05 -0500

    nuevo commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  
+  entry: [
+    './src/app.js'
+  ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'src/pages/index.html',
+      template: 'src/pages/index.html',
+      inject: true
+    })
+  ]
+}
+
+/*
+'use strict'
+const { VueLoaderPlugin } = require('vue-loader')
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
+*/
